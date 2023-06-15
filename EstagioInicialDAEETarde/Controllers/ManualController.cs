d21b262530d5168a5f161721252ce86617ce60ea
eststitardedes <eststitardedes@STI-ESTAGIARIO>
2023-06-15 15:47:14 -0300

    Adicionar arquivos de projeto.

---
+++ EstagioInicialDAEETarde/Controllers/ManualController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvcMysql.Controllers
+{
+    public class ManualController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
